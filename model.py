2c8cfb04744ae9dfef2b99ab49c20c2d9de7da99
nikhilgpt23 <47269041+nikhilgpt23@users.noreply.github.com>
2020-07-05 16:54:21 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -1,53 +1,39 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul  2 14:56:32 2020
-
-@author: NIKHIL
-"""
-
-
+# Importing the libraries
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 import pickle
 
 dataset = pd.read_csv('hiring.csv')
 
-dataset['experience'].fillna(0,inplace = True)
+dataset['experience'].fillna(0, inplace=True)
 
-dataset['test_score'].fillna(dataset['test_score'].mean(),inplace = True)
+dataset['test_score'].fillna(dataset['test_score'].mean(), inplace=True)
 
-X = dataset.iloc[:,:3]
+X = dataset.iloc[:, :3]
 
+#Converting words to integer values
 def convert_to_int(word):
-    word_dict = {'one'    : 1,
-                 'two'    : 2,
-                 'three'  : 3,
-                 'four'   : 4,
-                 'five'   : 5,
-                 'six'    : 6,
-                 'seven'  : 7,
-                 'eight'  : 8,
-                 'nine'   : 9,
-                 'ten'    : 10,
-                 'eleven' : 11,
-                 'twelve' : 12,
-                 'zero'   : 0,
-                 '0'      : 0}
+    word_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8,
+                'nine':9, 'ten':10, 'eleven':11, 'twelve':12, 'zero':0, 0: 0}
     return word_dict[word]
 
 X['experience'] = X['experience'].apply(lambda x : convert_to_int(x))
 
-y = dataset.iloc[:,-1]
+y = dataset.iloc[:, -1]
+
+#Splitting Training and Test Set
+#Since we have a very small dataset, we will train our model with all availabe data.
 
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 
-regressor.fit(X,y)
+#Fitting model with trainig data
+regressor.fit(X, y)
 
-pickle.dump(regressor,open('model.pkl','wb'))
+# Saving model to disk
+pickle.dump(regressor, open('model.pkl','wb'))
 
+# Loading model to compare the results
 model = pickle.load(open('model.pkl','rb'))
-
-print(model.predict([[9,6,2]]))
-
+print(model.predict([[2, 9, 6]]))
